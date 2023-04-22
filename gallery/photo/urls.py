9b115b219bf089833ea2eff9acfe418c4e603370
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-22 12:59:42 +0600

    #1 Этап 2. CRUD операции
    Добавил форму для модели Photo
    Добавил urls
    Добавил вывод информации в шаблонах

--- gallery/photo/urls.py
+++ gallery/photo/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
 
-urlpatterns = [
+from photo.views.index import IndexView
 
+from photo.views.photo import PhotoCreateView, PhotoUpdateView, PhotoDetailView, PhotoDeleteView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('photo/create/', PhotoCreateView.as_view(), name='photo_create'),
+    path('photo/<int:pk>/update/', PhotoUpdateView.as_view(), name='photo_update'),
+    path('photo/<int:pk>', PhotoDetailView.as_view(), name='photo_detail'),
+    path('photo/<int:pk>/delete', PhotoDeleteView.as_view(), name='photo_delete'),
 ]
---
+++ gallery/photo/views/photo.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views import View
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+
+from photo.models import Photo
+
+from photo.forms import PhotoForm
+
+
+class PhotoCreateView(LoginRequiredMixin, CreateView):
+    template_name = 'photo/photo_create.html'
+    model = Photo
+    form_class = PhotoForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('photo_detail', kwargs={'pk': self.object.pk})
+
+
+class PhotoDetailView(DetailView):
+    template_name = 'photo/photo_detail.html'
+    model = Photo
+
+
+class PhotoUpdateView(UpdateView):
+    template_name = 'photo/photo_update.html'
+    model = Photo
+    form_class = PhotoForm
+
+    def get_success_url(self):
+        return reverse('photo_detail', kwargs={'pk': self.object.pk})
+
+
+class PhotoDeleteView(DeleteView):
+    model = Photo
+
+    def get_success_url(self):
+        return reverse('profile', kwargs={'pk': self.object.author_id})
